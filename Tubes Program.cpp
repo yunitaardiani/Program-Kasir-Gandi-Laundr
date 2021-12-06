87eff088861d78403ce05bb62b5b5f90c4467937
yunitaardiani <94882829+yunitaardiani@users.noreply.github.com>
2021-12-06 22:28:57 +0800

    Update Tubes Program.cpp

--- Tubes Program.cpp
+++ Tubes Program.cpp
@@ -21,4 +21,66 @@ int main ()
 	//TANGGAL
 	time_t t = time(NULL);
 	struct tm tm = *localtime(&t);
+	
+	
+	
+	
+	printf("|   =================== SELAMAT DATANG ==================  |\n");
+	printf("|               PROGRAM KASIR GANDI LAUNDRY                |\n");
+	printf("============================================================\n");
+
+	printf("============================================================\n");
+	printf("|  ==================== LOGIN ADMIN ====================   |\n");
+	kembali:
+	printf("Username : ");
+	scanf("%s", username);
+	printf("Password : ");
+	scanf("%s", password);
+	printf("\n=============================================================\n");
+
+	if(strcmp(username, "gandi_laundry")==0 && strcmp(password,"12345")==0)
+	{
+		printf("\nID operator 		: 1\n");
+		printf("Nama Operator 		: Operator 1\n");
+		printf("\nAkses diterima. Selamat Datang Di Program Kasir Gandi Laundry\n");
+    	}
+
+
+   	 else
+	{
+    	printf("\nUsername dan Password tidak sesuai\n");
+    	printf("Tekan Y untuk kembali atau tekan T untuk keluar : ");
+	scanf("%s", &pilihan);
+
+
+		if (pilihan== 'Y' || pilihan== 'y')
+		{
+			goto kembali;
+		}
+		else if (pilihan== 'T' || pilihan== 't')
+    
+		{
+			goto keluar;
+		}
+	}
+
+	printf("\n\nTekan Y untuk kembali atau tekan T untuk keluar : ");
+	scanf("%s", &pilihan);
+
+		if (pilihan== 'Y' || pilihan== 'y')
+		{
+			goto kembali;
+		}
+		else if (pilihan== 'T' || pilihan== 't')
+		{
+			goto keluar;
+		}
+
+	keluar : 
+
+  
+	printf("\nTerimakasih Telah Mempercayai Gandi Laundry!\n"); 
+		return 0;
+
+}
 
